7fa1b3e78752f2aa39fa9208dfb93ef57dc42311
mohamed <mohamed@gmail.com>
2024-06-27 16:07:47 +0200

    add exercice react - spring security

---
+++ spring-security/exercice-react-spring-security/tp_spring_security_react/todo-ui/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>Copyrights reserved at 2023-25 by App CDA</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
