7fabfb4b21fa5364f541d9046f6b1a09cada3459
mohamed <mohamed@gmail.com>
2024-06-27 10:48:17 +0200

    add change userservice

--- spring-security/spring-security-product-jwt/src/main/java/com/example/jwtspringsecurity/controller/UserController.java
+++ spring-security/spring-security-product-jwt/src/main/java/com/example/jwtspringsecurity/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.jwtspringsecurity.controller;
 
 
 import com.example.jwtspringsecurity.dto.BaseResponseDto;
+import com.example.jwtspringsecurity.dto.UserLoginDto;
 import com.example.jwtspringsecurity.model.User;
 import com.example.jwtspringsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -31,9 +35,22 @@ public class UserController {
     }
 
 
+    @PostMapping("/login")
+    public BaseResponseDto loginUser(@RequestBody UserLoginDto loginDto){
+        if(userService.checkUserNameExists(loginDto.getEmail())){
+            if(userService.verifyUser(loginDto.getEmail(), loginDto.getPassword())){
+                Map<String, String> data = new HashMap<>();
+                data.put("token", userService.generateToken(loginDto.getEmail(), loginDto.getPassword()));
+                return new BaseResponseDto("Success",data);
 
+            }else{
+                return new BaseResponseDto("wrong password");
+            }
+        }else{
+            return new BaseResponseDto("User do not exist");
+        }
 
-   // @PostMapping("/login")
+    }
 
 
 
